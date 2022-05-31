86177408c81cf3fb2b8a3b8bf39f3d0cc8fbefe4
XMunteR <danielal19022003@gmail.com>
2022-05-30 23:01:33 -0500

    Bases de Datos Changes

--- Login.php
+++ Login.php
@@ -7,8 +7,27 @@
 	<title>Login</title>
 </head>
 <body>
-	<div class="login-side-bar"></div>
-
+	
+	
+	<div class="login-side-bar">
+		<p class="login-Admin-text">Ingreso de datos Administrador</p>
+	</div>
+	
 	<img class="login-head" src="img/login-head.png">
+
+	<label class="login-signin">Sign In</label>
+
+	<label class="login-Username">Nombre</label>
+	<input class="login-Usuarme-txt" type="text" name="Usuarme">
+
+	<label class="login-Password">Contraseña</label>
+	<input class="login-Password-txt" type="password" name="Password">
+
+
+	<a class="login-signUp" href="Registro.php">Sign Up</a>
+
+
+
+	
 </body>
 </html>
---
+++ Registro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/Style.css">
+	<title>Registro de datos</title>
+</head>
+<body>
+	<div class="Registro-top-Banner">
+		<p></p>
+	</div>
+</body>
+</html>
