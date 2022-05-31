d365b582fe2801c88368702235d6d6cd63c27216
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-05-31 00:02:42 -0500

    a

--- VisualizeExp.php
+++ VisualizeExp.php
@@ -3,9 +3,30 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/Style.css">
 	<title>Experimentos</title>
 </head>
 <body>
+	<div class="Experimentos-top-bar">
+	<p class="Experimentos-top-text">Datos experimento social (conformidad)</p>
+	</div>
 
+<table>
+	<TR>
+		<td><label>Nombre:</label></td>
+		<td><label><input type="text" name="Nombre" /></label></td>
+	</TR>
+</table>
+<table class="Table-view">
+
+	<tr>
+		<td>ID</td>
+		<td>Fecha</td>
+		<td>Hora</td>
+		<td>Validacion</td>
+		<td>Tiempo</td>
+
+		<td>Observaciones</td>
+	</tr>
 </body>
 </html>
