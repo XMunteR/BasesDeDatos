4de5226dbd367b5578cf7ec9b2d20519c2443ede
XMunteR <danielal19022003@gmail.com>
2022-05-31 00:26:16 -0500

    Update de las 12:25- 31/05hora en la que me retiro

--- Registro.php
+++ Registro.php
@@ -2,13 +2,37 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/Style.css">
 	<title>Registro de datos</title>
 </head>
 <body>
 	<div class="Registro-top-Banner">
-		<p></p>
+		<p class="Registro-top-text">Registro de Datos</p>
+	</div>
+	
+	<div class="Registro-top-Exp">
+		<p class="Registro-topBox-Exp">Experimento</p>
+		<div class="Registro-box-Exp">
+
+			<p class="Registro-box-content-lugar">Lugar</p>
+			<input class="Registro-box-Exp-lugar" type="text" name="Place">
+
+			<p class="Registro-box-content-Fecha">Fecha</p>
+			<input class="Registro-box-Exp-Date" type="Date" name="Date">
+
+			<p class="Registro-box-content-Hora">Hora</p>
+			<input class="Registro-box-Exp-Time" type="text" name="Time">
+
+			<button class="Registro-box-Exp-btn" name="send-exp">Enviar</button>
+		</div>
+	</div>
+
+	<div class="Registro-top-Reg">
+	<p class="Registro-topBox-Reg">Registro</p>
+		<div class="Registro-box-Reg">
+			<p class="Registro-box-Reg-Sujeto">Sujeto #</p>
+		</div>
 	</div>
 </body>
 </html>
